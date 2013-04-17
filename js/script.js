80c2130c6bc0362b3fa6df9168fde48d00c80ed9
Jacob Flohre <jake@heysparkbox.com>
2013-04-17 13:11:10 -0400

    Final Version

--- js/script.js
+++ js/script.js
@@ -19,129 +19,25 @@ APP = {
     // For numbers being converted to telephone operator in Opera
     $('a[href ^="tel:"]').removeAttr('href');
     // start jqtweet!
-    JQTWEET.loadTweets();
+    getTweets();
   }
 
 };
 
-// For twitter API
-JQTWEET = {
-  // Set twitter username, number of tweets & id/class to append tweets
-  user: 'DaytonCleanCode',
-  numTweets: 4,
-  appendTo: '#jstwitter',
-  // core function of jqtweet
-  loadTweets: function() {
-  $.ajax({
-    url: 'http://api.twitter.com/1/statuses/user_timeline.json/',
-    type: 'GET',
-    dataType: 'jsonp',
-    data: {
-      screen_name: JQTWEET.user,
-      include_rts: true,
-      count: JQTWEET.numTweets,
-      include_entities: true
-    },
-    success: function(data, textStatus, xhr) {
-      var html = '<div class="tweet">TWEET_TEXT<div class="time">AGO</div>';
-      // append tweets into page
-      for (var i = 0; i < data.length; i++) {
-        $(JQTWEET.appendTo).append(
-        html.replace('TWEET_TEXT', JQTWEET.ify.clean(data[i].text) )
-          .replace(/USER/g, data[i].user.screen_name)
-          .replace('AGO', JQTWEET.timeAgo(data[i].created_at) )
-          .replace(/ID/g, data[i].id_str)
-        );
+// Chirp.js on github
+function getTweets(){
+  var c = new Chirp({
+    target: 'tweets',
+    user:'DaytonCleanCode',
+    max:4,
+    templates: {
+        base: '<ul class="chirp">{{tweets}}</ul>',
+        tweet: '<li><p><a href="http://twitter.com/{{user.screen_name}}" title="{{user.name}} — {{user.description}}"><img src="{{user.profile_image_url}}"></a> {{html}}</p><span class="meta"><time><a href="http://twitter.com/{{user.screen_name}}/statuses/{{id_str}}">{{time_ago}}</a></time> &mdash; via <a href="http://twitter.com/{{user.screen_name}}" title="{{user.name}} — {{user.description}}">{{user.name}}</a></span></li>'
       }
-    }
   });
-  },
+  c.show();
+}
 
-  /**
-    * relative time calculator FROM TWITTER
-    * @param {string} twitter date string returned from Twitter API
-    * @return {string} relative time like "2 minutes ago"
-    */
-  timeAgo: function(dateString) {
-    var rightNow = new Date();
-    var then = new Date(dateString);
-      if ($.browser.msie) {
-        // IE can't parse these crazy Ruby dates
-        then = Date.parse(dateString.replace(/( \+)/, ' UTC$1'));
-      }
-    var diff = rightNow - then;
-    var second = 1000,
-    minute = second * 60,
-    hour = minute * 60,
-    day = hour * 24,
-    week = day * 7;
-      if (isNaN(diff) || diff < 0) {
-        return ""; // return blank string if unknown
-      }
-      if (diff < second * 2) {
-        // within 2 seconds
-        return "right now";
-      }
-      if (diff < minute) {
-        return Math.floor(diff / second) + " seconds ago";
-      }
-      if (diff < minute * 2) {
-        return "about 1 minute ago";
-      }
-      if (diff < hour) {
-        return Math.floor(diff / minute) + " minutes ago";
-      }
-      if (diff < hour * 2) {
-        return "about 1 hour ago";
-      }
-      if (diff < day) {
-        return  Math.floor(diff / hour) + " hours ago";
-      }
-      if (diff > day && diff < day * 2) {
-        return "yesterday";
-      }
-      if (diff < day * 365) {
-        return Math.floor(diff / day) + " days ago";
-      }
-      else {
-        return "over a year ago";
-      }
-  }, // timeAgo()
-
-
-  /**
-    * The Twitalinkahashifyer!
-    * http://www.dustindiaz.com/basement/ify.html
-    * Eg:
-    * ify.clean('your tweet text');
-    */
-  ify:  {
-    link: function(tweet) {
-      return tweet.replace(/\b(((https*\:\/\/)|www\.)[^\"\']+?)(([!?,.\)]+)?(\s|$))/g, function(link, m1, m2, m3, m4) {
-        var http = m2.match(/w/) ? 'http://' : '';
-        return '<a class="twtr-hyperlink" target="_blank" href="' + http + m1 + '">' + ((m1.length > 25) ? m1.substr(0, 24) + '...' : m1) + '</a>' + m4;
-      });
-    },
-    at: function(tweet) {
-      return tweet.replace(/\B[@＠]([a-zA-Z0-9_]{1,20})/g, function(m, username) {
-        return '<a target="_blank" class="twtr-atreply" href="http://twitter.com/intent/user?screen_name=' + username + '">@' + username + '</a>';
-      });
-    },
-    list: function(tweet) {
-      return tweet.replace(/\B[@＠]([a-zA-Z0-9_]{1,20}\/\w+)/g, function(m, userlist) {
-        return '<a target="_blank" class="twtr-atreply" href="http://twitter.com/' + userlist + '">@' + userlist + '</a>';
-      });
-    },
-    hash: function(tweet) {
-      return tweet.replace(/(^|\s+)#(\w+)/gi, function(m, before, hash) {
-        return before + '<a target="_blank" class="twtr-hashtag" href="http://twitter.com/search?q=%23' + hash + '">#' + hash + '</a>';
-      });
-    },
-    clean: function(tweet) {
-      return this.hash(this.at(this.list(this.link(tweet))));
-    }
-  } // ify    
-};
 
 $(document).ready(APP.init);
 // For media queries
@@ -160,3 +56,4 @@ $(window).load(function() {
     }
   });
 });
+
